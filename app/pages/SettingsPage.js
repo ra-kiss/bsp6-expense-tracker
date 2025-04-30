6b086d146dddf197b407f794c1fff25206bb74d5
Robert-Alexandru Kiss <robertkiss@onmail.com>
2025-04-30 12:17:25 +0200

    Edit main currency added

--- app/pages/SettingsPage.js
+++ app/pages/SettingsPage.js
@@ -7,8 +7,11 @@ import SettingsTopBar from '../components/settings/SettingsTopBar';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardActionArea from '@mui/material/CardActionArea';
+import Select from '@mui/material/Select';
 import Typography from '@mui/material/Typography';
 import MenuIcon from '@mui/icons-material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { useGlobal } from '../components/GlobalContext';
 
 import CategoriesModal from '../components/settings/CategoriesModal';
 
@@ -18,7 +21,8 @@ export default function SettingsPage() {
   const descriptionStyle = { color: 'text.secondary', fontSize: 13, maxWidth: 300 };
   
   const [categoriesModalOpen, setCategoriesModalOpen] = useState(false);
-    
+  const { currencies, mainCurrency, setMainCurrency } = useGlobal();
+  
   return (
     <>
       <SettingsTopBar/>
@@ -32,11 +36,20 @@ export default function SettingsPage() {
                   <Typography sx={{ fontSize: 16 }} className="font-bold" component="div">
                     Main Currency
                   </Typography>
-                  <Typography sx={descriptionStyle}>
-                  Choose which currency is used to calculate exchange rates and display your dashboard
+                  <Typography sx={{...descriptionStyle, maxWidth: 250 }}>
+                  Used for exchange rates and Home
                   </Typography>
                 </div>
-                <MenuIcon/>
+                <Select
+                  value={mainCurrency}
+                  onChange={(event) => setMainCurrency(event.target.value)}
+                >
+                  {currencies.map((currency) => (
+                    <MenuItem key={currency.value} value={currency.value}>
+                      {currency.label}
+                    </MenuItem>
+                  ))}
+                </Select>
               </div>
             </CardContent>
           </CardActionArea>
